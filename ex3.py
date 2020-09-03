50528d79da9f01221a5590cb6dfda89d383aaeb6
Amrutha <62595123+Amrutha26@users.noreply.github.com>
2020-09-03 14:48:13 +0530

    String Formatting

---
+++ ex3.py
@@ -0,0 +1,11 @@
+print("Hello {}, your balance is {}.".format("Amrutha", 50))
+
+print("Hello {0}, your balance is {1}.".format("Amrutha", 50))
+
+print("Hello {name}, your balance is {amount}.".format(name="Amrutha", amount=50))
+
+print("Hello {0}, your balance is {amount}.".format("Amrutha", amount=50))
+
+name = 'Amrutha'
+amount = 50
+print(f"Hello {name}, your balance is {amount}.")
